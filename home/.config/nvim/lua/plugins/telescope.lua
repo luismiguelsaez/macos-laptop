621564ef70abdddda8db23a2b4844f2fc999dc43
luismiguelsaez <luismiguelsaez@protonmail.com>
2024-06-08 10:37:03 +0200

    Fix

--- home/.config/nvim/lua/plugins/telescope.lua
+++ home/.config/nvim/lua/plugins/telescope.lua
@@ -1,5 +1,3 @@
--- local lga_actions = require("telescope-live-grep-args.actions")
-
 return {
   "nvim-telescope/telescope.nvim",
   enabled = true,
@@ -45,22 +43,22 @@ return {
         -- builtin picker
       },
       extensions = {
-        -- live_grep_args = {
-        --   auto_quoting = true, -- enable/disable auto-quoting
-        --   -- define mappings, e.g.
-        --   mappings = { -- extend mappings
-        --     i = {
-        --       ["<C-k>"] = lga_actions.quote_prompt(),
-        --       ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
-        --       -- freeze the current list and start a fuzzy search in the frozen list
-        --       -- ["<C-space>"] = actions.to_fuzzy_refine,
-        --     },
-        --   },
-        --   -- ... also accepts theme settings, for example:
-        --   -- theme = "dropdown", -- use dropdown theme
-        --   -- theme = { }, -- use own theme spec
-        --   -- layout_config = { mirror=true }, -- mirror preview pane
-        -- },
+        live_grep_args = {
+          auto_quoting = true, -- enable/disable auto-quoting
+          -- define mappings, e.g.
+          mappings = { -- extend mappings
+            i = {
+              ["<C-k>"] = require("telescope-live-grep-args.actions").quote_prompt(),
+              ["<C-i>"] = require("telescope-live-grep-args.actions").quote_prompt({ postfix = " --iglob " }),
+              -- freeze the current list and start a fuzzy search in the frozen list
+              ["<C-space>"] = require("telescope-live-grep-args.actions").to_fuzzy_refine,
+            },
+          },
+          -- ... also accepts theme settings, for example:
+          -- theme = "dropdown", -- use dropdown theme
+          -- theme = { }, -- use own theme spec
+          -- layout_config = { mirror=true }, -- mirror preview pane
+        },
       },
     })
     -- require("telescope").load_extension("fzy_native")
