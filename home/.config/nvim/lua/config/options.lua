fb66043c72b1a761987d287c4c02cfc70bc8e3ed
luismiguelsaez <luismiguelsaez@protonmail.com>
2024-06-11 04:59:51 +0200

    Added key bindings

--- home/.config/nvim/lua/config/options.lua
+++ home/.config/nvim/lua/config/options.lua
@@ -7,10 +7,12 @@ local map = vim.keymap.set
 local defaults = { noremap = true, silent = true }
 
 -- NeoTree mappings
-map("n", "<leader>et", ":Neotree focus<cr>", defaults)
-map("n", "<leader>ef", ":Neotree float<cr>", defaults)
-map("n", "<leader>el", ":Neotree left<cr>", defaults)
-map("n", "<leader>eb", ":Neotree bottom<cr>", defaults)
+map("n", "<leader>ef", ":Neotree focus<cr>", defaults)
+map("n", "<leader>et", ":Neotree toggle<cr>", defaults)
+map("n", "<leader>el", ":Neotree last<cr>", defaults)
+map("n", "<leader>eF", ":Neotree float<cr>", defaults)
+map("n", "<leader>eL", ":Neotree left<cr>", defaults)
+map("n", "<leader>eB", ":Neotree bottom<cr>", defaults)
 map("n", "<leader>eg", ":Neotree git_status<cr>", defaults)
 
 -- Telescope mappings
