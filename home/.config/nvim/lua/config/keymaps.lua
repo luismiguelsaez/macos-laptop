7ebd160981015eb6a0c6180f066649cc2d279436
luismiguelsaez <luismiguelsaez@protonmail.com>
2024-06-13 18:22:09 +0200

    Relocate configs

--- home/.config/nvim/lua/config/keymaps.lua
+++ home/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,28 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Globals
+local map = vim.keymap.set
+local defaults = { noremap = true, silent = true }
+
+-- NeoTree mappings
+map("n", "<leader>ef", ":Neotree focus<cr>", defaults)
+map("n", "<leader>et", ":Neotree toggle<cr>", defaults)
+map("n", "<leader>el", ":Neotree last<cr>", defaults)
+map("n", "<leader>eF", ":Neotree float<cr>", defaults)
+map("n", "<leader>eL", ":Neotree left<cr>", defaults)
+map("n", "<leader>eB", ":Neotree bottom<cr>", defaults)
+map("n", "<leader>eg", ":Neotree git_status<cr>", defaults)
+
+-- Telescope mappings
+map(
+  "n",
+  "<leader>fG",
+  ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>",
+  { noremap = true, silent = true, desc = "Recursive grep" }
+)
+
+-- local telescope_builtin = require("telescope.builtin")
+-- vim.keymap.set("n", "<C-x>", telescope_builtin.find_files, {})
+-- vim.keymap.set("n", "<C-a>", telescope_builtin.live_grep, {})
--- home/.config/nvim/lua/config/options.lua
+++ home/.config/nvim/lua/config/options.lua
@@ -2,23 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- Globals
-local map = vim.keymap.set
-local defaults = { noremap = true, silent = true }
-
--- NeoTree mappings
-map("n", "<leader>ef", ":Neotree focus<cr>", defaults)
-map("n", "<leader>et", ":Neotree toggle<cr>", defaults)
-map("n", "<leader>el", ":Neotree last<cr>", defaults)
-map("n", "<leader>eF", ":Neotree float<cr>", defaults)
-map("n", "<leader>eL", ":Neotree left<cr>", defaults)
-map("n", "<leader>eB", ":Neotree bottom<cr>", defaults)
-map("n", "<leader>eg", ":Neotree git_status<cr>", defaults)
-
--- Telescope mappings
-map(
-  "n",
-  "<leader>fG",
-  ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>",
-  { noremap = true, silent = true, desc = "Recursive grep" }
-)
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+vim.g.mapleader = " "
