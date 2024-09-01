f4eaf43e2807d39282fdfd6226647c5798e6e363
luismiguelsaez <luismiguelsaez@protonmail.com>
2024-09-01 10:49:50 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,25 @@
 #!/usr/bin/env bash
 
+# Install oh-my-zsh and plugins
+
+if [ ! -e ~/.oh-my-zsh ]; then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+else
+  log_yellow "Skipping ohmyzsh install"
+fi
+
+if [[ ! -d $ZSH_CUSTOM/plugins/autoswitch_virtualenv ]]; then
+  git clone https://github.com/MichaelAquilina/zsh-autoswitch-virtualenv.git $ZSH_CUSTOM/plugins/autoswitch_virtualenv
+fi
+
+if [[ ! -d $ZSH_CUSTOM/plugins/zsh-autosuggestions ]]; then
+  git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+fi
+
+# Install oh-my-posh
+
+curl -s https://ohmyposh.dev/install.sh | bash -s -- -d ~/.local/bin
+
 # Install XCode
 
 xcode-select --install
@@ -11,3 +31,4 @@ brew bundle
 # Copy dotfiles, including hidden files
 
 cp -rp home/ ~/
+
