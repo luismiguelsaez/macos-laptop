2baafbc83d7e7ee8c9fad75026554fce94fdc84c
luismiguelsaez <luismiguelsaez@protonmail.com>
2024-09-11 17:41:11 +0200

    Updated which-key syntax

--- home/.config/nvim/lua/config/keymaps.lua
+++ home/.config/nvim/lua/config/keymaps.lua
@@ -7,13 +7,13 @@ local map = vim.keymap.set
 local defaults = { noremap = true, silent = true }
 
 -- NeoTree mappings
-map("n", "<leader>ef", ":Neotree focus<cr>", defaults)
-map("n", "<leader>et", ":Neotree toggle<cr>", defaults)
-map("n", "<leader>el", ":Neotree last<cr>", defaults)
-map("n", "<leader>eF", ":Neotree float<cr>", defaults)
-map("n", "<leader>eL", ":Neotree left<cr>", defaults)
-map("n", "<leader>eB", ":Neotree bottom<cr>", defaults)
-map("n", "<leader>eg", ":Neotree git_status<cr>", defaults)
+-- map("n", "<leader>ef", ":Neotree focus<cr>", defaults)
+-- map("n", "<leader>et", ":Neotree toggle<cr>", defaults)
+-- map("n", "<leader>el", ":Neotree last<cr>", defaults)
+-- map("n", "<leader>eF", ":Neotree float<cr>", defaults)
+-- map("n", "<leader>eL", ":Neotree left<cr>", defaults)
+-- map("n", "<leader>eB", ":Neotree bottom<cr>", defaults)
+-- map("n", "<leader>eg", ":Neotree git_status<cr>", defaults)
 
 -- Telescope mappings
 map(
@@ -26,3 +26,16 @@ map(
 -- local telescope_builtin = require("telescope.builtin")
 -- vim.keymap.set("n", "<C-x>", telescope_builtin.find_files, {})
 -- vim.keymap.set("n", "<C-a>", telescope_builtin.live_grep, {})
+
+local wk = require("which-key")
+wk.add({
+  mode = { "n" },
+  group = "Neotree",
+  { "<leader>ef", "<cmd>Neotree focus<cr>", desc = "Focus" },
+  { "<leader>et", "<cmd>Neotree toggle<cr>", desc = "Toggle" },
+  { "<leader>el", "<cmd>Neotree last<cr>", desc = "Last" },
+  { "<leader>eF", "<cmd>Neotree float<cr>", desc = "Float" },
+  { "<leader>eL", "<cmd>Neotree left<cr>", desc = "Left" },
+  { "<leader>eB", "<cmd>Neotree bottom<cr>", desc = "Botton" },
+  { "<leader>eg", "<cmd>Neotree git_status<cr>", desc = "Git status" },
+})
