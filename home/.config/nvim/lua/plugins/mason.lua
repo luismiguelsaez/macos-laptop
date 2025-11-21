ea66a7befe9e5ba74b6e17a9446b2d495fb7b6d4
luismiguelsaez <lmartin9@apple.com>
2025-11-21 13:54:43 +0100

    Update Mason

--- home/.config/nvim/lua/plugins/mason.lua
+++ home/.config/nvim/lua/plugins/mason.lua
@@ -30,6 +30,18 @@ return {
 
       lspconfig.pyright.setup({
         capabilities = capabilities,
+        single_file_support = true,
+        root_dir = function(fname)
+          -- Find project root based on these markers
+          return require("lspconfig.util").root_pattern(
+            "pyrightconfig.json",
+            "pyproject.toml",
+            "setup.py",
+            "setup.cfg",
+            "requirements.txt",
+            ".git"
+          )(fname) or vim.fn.getcwd()
+        end,
         on_init = function(client)
           local venv_path = vim.fn.getcwd() .. "/.venv"
           if vim.fn.isdirectory(venv_path) == 1 then
@@ -41,8 +53,11 @@ return {
           python = {
             analysis = {
               autoSearchPaths = true,
-              diagnosticMode = "workspace",
+              diagnosticMode = "openFilesOnly", -- Back to openFilesOnly for speed
+              useLibraryCodeForTypes = true,
               typeCheckingMode = "basic",
+              reportPrivateImportUsage = false,
+              reportAssignmentType = false,
             },
           },
         },
@@ -59,13 +74,27 @@ return {
               select = { "E", "W", "F", "I", "N", "UP", "B", "C4", "SIM" },
               ignore = { "E501" },
             },
-            format = { enable = true },
+            format = {
+              enable = true,
+            },
             organizeImports = true,
             fixAll = true,
           },
         },
         on_attach = function(client, bufnr)
           client.server_capabilities.hoverProvider = false
+
+          -- Auto-format and organize imports on save
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            buffer = bufnr,
+            callback = function()
+              vim.lsp.buf.code_action({
+                context = { only = { "source.organizeImports" } },
+                apply = true,
+              })
+              vim.lsp.buf.format({ name = "ruff" })
+            end,
+          })
         end,
       })
     end,
